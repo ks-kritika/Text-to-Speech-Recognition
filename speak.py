4be1a9078765a510765d275958f4bdd75036d4e9
Kritika Sinha <86712721+ks-kritika@users.noreply.github.com>
2021-07-09 21:17:26 +0530

    Add files via upload

---
+++ speak.py
@@ -0,0 +1,32 @@
+import pyttsx3                      #library to convert text into speech
+engine = pyttsx3.init()             #object creation
+
+rate = engine.getProperty('rate')
+print(rate)
+engine.setProperty('rate', 180)
+volume = engine.getProperty('volume')
+print(volume)
+engine.setProperty('volume', 1000)
+voices = engine.getProperty('voices')
+print(voices)
+engine.setProperty('voice', voices[0].id)   #for male
+
+engine.say("I LOVE YOU")
+engine.runAndWait()
+
+engine.setProperty('volume', 50000)
+engine.setProperty('rate', 100)
+voices=engine.getProperty('voices')
+print(voices)
+
+engine.setProperty('voice', voices[1].id)  #for female
+
+engine.say("I LOVE YOU MORE")
+engine.runAndWait()
+
+with open('all lang') as f:
+    for item in f.readlines():
+        engine.say(item)
+engine.runAndWait()
+
+engine.stop()
